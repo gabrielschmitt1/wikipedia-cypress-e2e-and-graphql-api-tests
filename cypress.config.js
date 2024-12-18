b5ea51f28e0555b3a0576564f90aab723c8ce15e
Gabriel Schmitt <gabrielschmitt7@gmail.com>
2024-12-18 19:57:20 -0300

    (feat): change default timeout and add retries

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
   e2e: {
     viewportWidth: 1280,
     viewportHeight: 800,
+    defaultCommandTimeout: 10000,
     baseUrl: "https://pt.wikipedia.org",
     setupNodeEvents(on, config) {
       // implement node event listeners here
